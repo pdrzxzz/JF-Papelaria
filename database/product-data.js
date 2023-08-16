96b95d4124f484e5c21b639c4ecd7adfa345b4e4
Emanuel Pedroza <emanuelsalgado007@gmail.com>
2023-08-16 20:50:25 -0300

    change website color, update a bunch of styles, add some pens, add price

--- database/product-data.js
+++ database/product-data.js
@@ -1,15 +1,31 @@
 // todos os dados de produtos existentes
 const data = [
     {
-        img: "./images/caneta-bic-cristal-azul.webp",
-        name: "Caneta Bic Cristal Azul",
+        img: "./images/caneta-bic-cristal.webp",
+        name: "Caneta Bic Cristal 1.0mm",
         brand: 'bic',
-        type: "canetas"
+        type: "canetas",
+        price: '1,50'
     },
     {
-        img: "./images/caneta-bic-cristal-vermelha.webp",
-        name: "Caneta Bic Cristal Vermelha",
+        img: "./images/caneta-bic-fine.webp",
+        name: "Caneta Bic Fine 0.8mm",
         brand: 'bic',
-        type: "canetas"
+        type: "canetas",
+        price: '1,50'
+    },
+    {
+        img: "./images/caneta-bic-bold.webp",
+        name: "Caneta Bic Bold Intenso 1.6mm",
+        brand: 'bic',
+        type: "canetas",
+        price: '2,95'
+    },
+    {
+        img: "./images/caneta-bic-ultrafine.webp",
+        name: "Caneta Bic Ultra Fine 0.7mm",
+        brand: 'bic',
+        type: "canetas",
+        price: '3,95'
     }
 ]
--- scripts/script.js
+++ scripts/script.js
@@ -8,27 +8,46 @@ form.addEventListener('click', () => {
     productsDiv.innerHTML = ''
     productHeader.innerHTML = ''
     for (productLabel of form) {
+        //procurar qual produto foi clicado
         if (productLabel.checked === true) {
             productSelected = productLabel.value
+            //adicionar heading
             SectionHeading = document.createElement('h2')
-            SectionHeading.textContent = productLabel.id
+            SectionHeading.textContent = productLabel.id.replaceAll('-', ' ')
+            SectionHeading.classList.add('section-heading')
             productHeader.append(SectionHeading)
             for (product of data) {
-                // filtrar os produtos
+                //filtrar os produtos da database
                 if (product.type == productSelected) {
                     //adicionar os filtrados na tela
                     newProductSection = document.createElement('section')
                     newProductSection.classList.add('product-section')
+                    newProductSection.width = '50px'
+                    newProductImgAnchor = document.createElement('a')
+                    newProductImgAnchor.classList.add('img-anchor')
+                    newProductImgAnchor.target = '_blank'
+                    newProductImgAnchor.href = product.img
                     newProductImg = document.createElement('img')
                     newProductImg.src = product.img
                     newProductImg.classList.add('product-img')
+                    newProductImgAnchor.append(newProductImg)
                     newProductName = document.createElement('h3')
                     newProductName.textContent = product.name
-                    newProductSection.append(newProductImg, newProductName)
+                    newProductName.classList.add('product-name')
+                    newProductPrice = document.createElement('h4')
+                    newProductPrice.textContent = 'R$ ' + product.price
+                    newProductPrice.classList.add('product-price')
+                    newProductSection.addEventListener('click', grow)
+                    newProductSection.append(newProductImgAnchor, newProductName, newProductPrice)
                     newProductSection.classList.add('animation')
                     productsDiv.append(newProductSection)
+                    productHeader.append(productsDiv)
                 }
             }
         }
     }
 })
+
+function grow() {
+    console.log('a')
+}
