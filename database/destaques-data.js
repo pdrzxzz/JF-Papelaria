238f26e6da95d93d53a8cf081481a08d0972559a
Emanuel Pedroza <emanuelsalgado007@gmail.com>
2023-09-27 22:28:49 -0300

    actual status

---
+++ database/destaques-data.js
@@ -0,0 +1,10 @@
+const destaquesData = [
+    {
+        img: "./images/marca-texto-pastel-bic.jpg",
+        name: "Resma de papel ofício A4",
+        brand: 'Chamex',
+        type: "papel",
+        price: '25,00',
+        falta: false,
+    }
+]
--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,123 @@
 productsDiv = document.querySelector('#products')
 productHeader = document.querySelector('#product-header')
 form = document.querySelector('#products-form')
-carrinho = document.querySelector('#carrinho')
 let productSelected = ''
+let categorias = document.querySelector('#categorias')
+let todos = document.querySelector('#todos-produtos')
+let destaques = document.querySelector('#destaques')
 
+destaques.addEventListener('click', () => {
+    // if (SectionHeading) {
+    // SectionHeading.textContent = 'Destaques'
+    // }
+    form.classList.add('none')
+    for (product of productsDiv.children) {
+        productsDiv.remove(product)
+    }
+    let productsList = []
+    for (product of destaquesData) {
+        productsList.push(product)
+    }
+    //ordenar produtos por preço
+    productsList.sort(sortPrice)
+    for (product of productsList) {
+        //adicionar os produtos na tela
+        let newProductSection = document.createElement('section')
+        newProductSection.classList.add('product-section')
+        newProductSection.width = '50px'
+        let newProductImgAnchor = document.createElement('a')
+        newProductImgAnchor.classList.add('img-anchor')
+        newProductImgAnchor.target = '_blank'
+        newProductImgAnchor.href = product.img
+        let newProductImg = document.createElement('img')
+        newProductImg.src = product.img
+        newProductImg.classList.add('product-img')
+        newProductImgAnchor.append(newProductImg)
+        let newProductName = document.createElement('h2')
+        newProductName.textContent = product.name
+        newProductName.classList.add('product-name')
+        let newProductBio = document.createElement('div')
+        newProductBio.classList.add('product-bio')
+        let newProductBrand = document.createElement('h3')
+        newProductBrand.textContent = 'Marca: ' + product.brand
+        newProductBrand.classList.add('product-brand')
+        let newProductPrice = document.createElement('h2')
+        if (product.falta == true) {
+            newProductPrice.textContent = 'EM FALTA'
+            newProductSection.style.opacity = 0.5
+        }
+        else {
+            productPrice = product.price.replace('.', ',')
+            newProductPrice.textContent = 'R$ ' + productPrice
+        }
+        newProductPrice.classList.add('product-price')
+        newProductBio.append(newProductName, newProductBrand, newProductPrice)
+        newProductSection.append(newProductImgAnchor, newProductBio)
+        newProductSection.classList.add('animation')
+        productsDiv.append(newProductSection)
+        productHeader.append(productsDiv)
+    }
+    productHeader.scrollIntoView({ behavior: 'smooth' })
+})
+
+todos.addEventListener('click', () => {
+    SectionHeading.textContent = ''
+    form.classList.add('none')
+    for (product of productsDiv.children) {
+        productsDiv.remove(product)
+    }
+    let productsList = []
+    for (product of data) {
+        productsList.push(product)
+    }
+    //ordenar produtos por preço
+    productsList.sort(sortPrice)
+    for (product of productsList) {
+        //adicionar os produtos na tela
+        let newProductSection = document.createElement('section')
+        newProductSection.classList.add('product-section')
+        newProductSection.width = '50px'
+        let newProductImgAnchor = document.createElement('a')
+        newProductImgAnchor.classList.add('img-anchor')
+        newProductImgAnchor.target = '_blank'
+        newProductImgAnchor.href = product.img
+        let newProductImg = document.createElement('img')
+        newProductImg.src = product.img
+        newProductImg.classList.add('product-img')
+        newProductImgAnchor.append(newProductImg)
+        let newProductName = document.createElement('h2')
+        newProductName.textContent = product.name
+        newProductName.classList.add('product-name')
+        let newProductBio = document.createElement('div')
+        newProductBio.classList.add('product-bio')
+        let newProductBrand = document.createElement('h3')
+        newProductBrand.textContent = 'Marca: ' + product.brand
+        newProductBrand.classList.add('product-brand')
+        let newProductPrice = document.createElement('h2')
+        if (product.falta == true) {
+            newProductPrice.textContent = 'EM FALTA'
+            newProductSection.style.opacity = 0.5
+        }
+        else {
+            productPrice = product.price.replace('.', ',')
+            newProductPrice.textContent = 'R$ ' + productPrice
+        }
+        newProductPrice.classList.add('product-price')
+        newProductBio.append(newProductName, newProductBrand, newProductPrice)
+        newProductSection.append(newProductImgAnchor, newProductBio)
+        newProductSection.classList.add('animation')
+        productsDiv.append(newProductSection)
+        productHeader.append(productsDiv)
+    }
+    productHeader.scrollIntoView({ behavior: 'smooth' })
+})
+
+categorias.addEventListener('click', () => {
+    for (product of productsDiv.children) {
+        productsDiv.remove(product)
+    }
+    form.classList.toggle('none')
+})
 
 form.addEventListener('click', () => {
     for (productLabel of form) {
@@ -42,25 +156,12 @@ form.addEventListener('click', () => {
                 let newProductName = document.createElement('h2')
                 newProductName.textContent = product.name
                 newProductName.classList.add('product-name')
+                let newProductBio = document.createElement('div')
+                newProductBio.classList.add('product-bio')
                 let newProductBrand = document.createElement('h3')
                 newProductBrand.textContent = 'Marca: ' + product.brand
                 newProductBrand.classList.add('product-brand')
                 let newProductPrice = document.createElement('h2')
-                let arrowDown = document.createElement('img')
-                arrowDown.src = 'logo/arrow-down.png'; arrowDown.classList.add('arrow-down')
-                let arrowUp = document.createElement('img')
-                arrowUp.src = 'logo/arrow-up.png'; arrowUp.classList.add('arrow-up')
-                let newBuyCount = document.createElement('input')
-                newBuyCount.type = 'number'; newBuyCount.id = 'number-count', newBuyCount.value = '1'
-                let counterSection = document.createElement('div')
-                counterSection.classList.add('counter-section')
-                let newBuyButton = document.createElement('div');
-                newBuyButton.classList.add('buy-button');
-                newBuyButton.textContent = 'Adicionar ao carrinho';
-                arrowUp.addEventListener('click', () => increaseNumber(product, newBuyCount))
-                arrowDown.addEventListener('click', () => decreaseNumber(product, newBuyCount))
-                newBuyButton.addEventListener('click', addCarrinho(product, newBuyCount));
-
                 if (product.falta == true) {
                     newProductPrice.textContent = 'EM FALTA'
                     newProductSection.style.opacity = 0.5
@@ -70,8 +171,8 @@ form.addEventListener('click', () => {
                     newProductPrice.textContent = 'R$ ' + productPrice
                 }
                 newProductPrice.classList.add('product-price')
-                counterSection.append(arrowDown, newBuyCount, arrowUp)
-                newProductSection.append(newProductImgAnchor, newProductName, newProductBrand, newProductPrice, counterSection, newBuyButton)
+                newProductBio.append(newProductName, newProductBrand, newProductPrice)
+                newProductSection.append(newProductImgAnchor, newProductBio)
                 newProductSection.classList.add('animation')
                 productsDiv.append(newProductSection)
                 productHeader.append(productsDiv)
@@ -82,112 +183,6 @@ form.addEventListener('click', () => {
 }
 )
 
-let total = 0
-let totalTracker = document.createElement('span')
-totalTracker.id = 'total-tracker'
-let copyButton = document.createElement('div')
-copyButton.id = 'copy-button'
-let carrinhoInfo = document.createElement('span')
-carrinhoInfo.id = 'carrinho-info'
-let carrinhoDiv = document.querySelector('#carrinho-div')
-carrinhoDiv.id = 'carrinho-div'
-
-let liDiv = document.createElement('div')
-liDiv.id = 'li-div'
-let productTotal = 0
-function addCarrinho(product, newBuyCount) {
-    return function () {
-        const number = parseInt(newBuyCount.value)
-        for (let i = 0; i < number; i++) {
-            if (!carrinho.querySelector('li')) {
-                product.quantity = 1;
-                total += parseFloat(product.price)
-                total = total.toFixed(2)
-                newLi = document.createElement('li')
-                newLi.classList.add('new-item')
-                newLi.innerHTML = `x${product.quantity} ${product.name} ${product.brand} | <b>R$${product.price.replace('.', ',')}</b>.\n`;
-                carrinhoInfo.textContent = 'Você pode copiar o conteúdo do seu carrinho e nos mandar via Whatsapp ou Instagram. Se a página for recarregada, o carrinho se esvaziará.'
-                totalTracker.innerHTML = `Total: R$<b>${total}</b>`
-                copyButton.textContent = 'Copiar'
-                liDiv.append(newLi)
-                carrinho.append(liDiv, totalTracker, copyButton)
-                carrinhoDiv.append(carrinho, carrinhoInfo)
-                totalTracker.innerHTML = totalTracker.innerHTML.replace('.', ',')
-            }
-            else {
-                //checar se o produto já está no carrinho
-                if (carrinho.textContent.includes(product.name)) {
-                    total = parseFloat(total)
-                    product.quantity += 1
-                    productTotal = (product.quantity * parseFloat(product.price)).toFixed(2)
-                    total += parseFloat(product.price)
-                    total = total.toFixed(2)
-                    for (li of liDiv.children) {
-                        if (li.textContent.includes(product.name)) {
-                            li.textContent = ''
-                            li.innerHTML += `x${product.quantity} ${product.name} ${product.brand} | R$${product.price.replace('.', ',')} * ${product.quantity} = <b>R$${productTotal.replace('.', ',')}</b>.\n`;
-                        }
-                    }
-                    totalTracker.innerHTML = `Total: R$<b>${total}</b>`
-                    totalTracker.innerHTML = totalTracker.innerHTML.replace('.', ',')
-                }
-                else {
-                    total = parseFloat(total)
-                    product.quantity = 1
-                    total += parseFloat(product.price)
-                    total = total.toFixed(2)
-                    newLi = document.createElement('li')
-                    newLi.classList.add('new-item')
-                    newLi.innerHTML = `x${product.quantity} ${product.name} ${product.brand} | <b>R$${product.price.replace('.', ',')}</b>.\n`;
-                    liDiv.append(newLi)
-                    carrinho.append(liDiv)
-                    totalTracker.innerHTML = `Total: R$<b>${total}</b>`
-                    totalTracker.innerHTML = totalTracker.innerHTML.replace('.', ',')
-                }
-            }
-        }
-    }
-}
-
 function sortPrice(a, b) {
     return a.price - b.price;
 }
-
-copyButton.addEventListener('click', () => {
-    navigator.clipboard.writeText(formatarCarrinho(carrinho))
-        .then(() => {
-            copyButton.textContent = 'Copiado! :)';
-            setTimeout(() => {
-                copyButton.textContent = 'Copiar';
-            }, 1250);
-        })
-        .catch((error) => {
-            console.error('Erro ao copiar para a área de transferência:', error);
-        });
-})
-
-function formatarCarrinho(text) {
-    let newText = text.textContent
-    newText = newText.replace(/🛒/g, '🛒\n\n');
-    newText = newText.replace(totalTracker.textContent, '')
-    newText = newText.replace(copyButton.textContent, '')
-    newText = newText.replaceAll('    ', '')
-    newText = newText + `\n${totalTracker.textContent}`
-    return newText;
-}
-
-function increaseNumber(product, newBuyCount) {
-    newBuyCount.value = parseInt(newBuyCount.value) + 1;
-}
-
-function decreaseNumber(product, newBuyCount) {
-    let number = parseInt(newBuyCount.value);
-    if (number > 1) {
-        newBuyCount.value = number - 1;
-    }
-}
-
-function primeiraLetraMaiuscula(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
